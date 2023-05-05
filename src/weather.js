e7c22fd58bc955dbc6aa18aa415aae356fd26620
aakashrajaraman <20btrcl069@jainuniversity.ac.in>
2023-05-05 14:52:24 +0530

    improved design

--- src/weather.js
+++ src/weather.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import './weather.css';
 
 function WeatherApp() {
   const [location, setLocation] = useState('');
@@ -7,28 +8,46 @@ function WeatherApp() {
   async function handleSubmit(event) {
     event.preventDefault();
     const apiKey = '828b5c16d63f6c2ca6a33524e780d7ba';
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=828b5c16d63f6c2ca6a33524e780d7ba`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=828b5c16d63f6c2ca6a33524e780d7ba`;
     const response = await fetch(apiUrl);
     const data = await response.json();
     setWeatherData(data);
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Enter a location:
-          <input type="text" value={location} onChange={(event) => setLocation(event.target.value)} />
-        </label>
-        <button type="submit">Get Weather</button>
-      </form>
-      {weatherData && (
-        <div>
-          <p>Temperature: {weatherData.main.temp}°C</p>
+    <div className="body">
+    <form onSubmit={handleSubmit}>
+      <label>
+        Enter a Location
+      </label>
+      <input type="text" value={location} onChange={(event) => setLocation(event.target.value)} />
+      <button type="submit">Get Weather</button>
+    </form>
+    {weatherData && (
+    <div className="Location">
+        <p>{weatherData.name}: {weatherData.weather[0].description}</p>
+    </div>
+    )}
+    
+    {weatherData && (
+        
+      <div className="weather-data">
+        
+        <div className="weather-info">
+          <p>Temperature: {weatherData.main.temp.toFixed()}°C</p>
+        </div>
+        <div className="weather-info">
+          <p>Feels Like: {weatherData.main.feels_like.toFixed()}°C</p>
+        </div>
+        <div className="weather-info">
+          <p>Visibility: {weatherData.visibility} meters</p>
+        </div>
+        <div className="weather-info">
           <p>Humidity: {weatherData.main.humidity}%</p>
         </div>
-      )}
-    </div>
+      </div>
+    )}
+  </div>
   );
 }
 
